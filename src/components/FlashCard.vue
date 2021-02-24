3299f2e66ea8e08a4cb76eca5cd377895872b0f9
Erik Lindstrom <elindstrom42@gmail.com>
2021-02-24 15:38:02 -0600

    cards showing and can click for answers, need formatting

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -1,1 +1,19 @@
-<template></template>
+<template>
+  <div>
+    <div v-if="!card.answerShown">
+      <h2>{{ card.question }}</h2>
+      <button @click="handleClick">Show Answer</button>
+    </div>
+    <div v-else>{{ card.answer }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["card"],
+  methods: {
+    handleClick() {
+      this.$emit("toggle", this.card);
+    },
+  },
+};
+</script>
